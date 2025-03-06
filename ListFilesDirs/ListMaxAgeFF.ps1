5742d9bae530b8f1b2cdf94fc19f1c251b142798
Ravi S. Iyer <riyer02@gmail.com>
2025-03-06 22:29:23 +0530

    Generalized ListMaxAgeFF

--- ListFilesDirs/ListMaxAgeFF.ps1
+++ ListFilesDirs/ListMaxAgeFF.ps1
@@ -1,14 +1,24 @@
-﻿# List last modified in x days, files and folders (excl. special folders) OR only unique parent folder names
+﻿# List last modified in x days, files and folders OR only unique parent folder names, (excl. special folders by default)
 # It uses ExcludeDirsRecurseListFilesDirs.ps1 to do the main work
 #
-param ($MaxAge="1", $ExcludeFoders="", $Unique="")
+param ($Path = $pwd, $MaxAge="1", $ExcludeFoders="", $Unique="")
 function Usage {
-  param ($cmdName)
-  Write-Host Usage: $cmdName [MaxAge] ['upf']
-  Write-Host Usage: If MaxAge is not specified`, default value of 1 [day] is used
-  Write-Host Usage: If second parameter is specified`, it has to be 'upf' [without quotes]. 
-  Write-Host Usage: If upf is specified`, only unique parent folder names are listed.
-  Write-Host Usage: If upf is not specified`, files and folders are listed.
+  param ($CmdName)
+  Write-Host "List last modified in x days, files and folders OR only unique parent folder names, (excl. special folders by default)"`n
+  Write-Host Usage: $CmdName [Path MaxAge Exclude-Folders-List UniqueParentFolder]`n
+  Write-Host This script relies on ExcludeDirsRecurseListFilesDirs.ps1 to do the work.
+  Write-Host Path specifies the input folder which if not specified has default value of . [current directory]
+  Write-Host If MaxAge is not specified`, default value of 1 [day] is used
+  Write-Host Exclude-Folders-List is passed as given to ExcludeDirsRecurseListFilesDirs.ps1
+  Write-Host If Exclude-Folders-List is omitted`, default value of `"`" is passed to ExcludeDirsRecurseListFilesDirs.ps1
+  Write-Host If UniqueParentFolder is specified`, it has to be 'upf' [without quotes] and then`, only unique parent folder names are listed.
+  Write-Host If UniqueParentFolder is not specified`, files and folders are listed.
+  Write-Host /? passed as first parameter shows this help message.
+}
+
+if ($Path -eq "/?") { 
+  Usage $myInvocation.InvocationName
+  exit 0
 }
 
 # Ref: https://collectingwisdom.com/powershell-check-if-variable-is-number/
@@ -29,11 +39,9 @@ if ($Unique -eq "") {
 }
 
 if ($Unique -eq "upf") {
-  ExcludeDirsRecurseListFilesDirs.ps1 | Where-Object { ((get-date)-$_.LastWriteTime).days -lt $MaxAge } `
+  ExcludeDirsRecurseListFilesDirs.ps1 $Path $ExcludeFoders | Where-Object { ((get-date)-$_.LastWriteTime).days -lt $MaxAge } `
   | ForEach-Object {Split-Path -Parent $_.FullName} | Select-Object -unique 
-
 } else {
-  ExcludeDirsRecurseListFilesDirs.ps1 | Where-Object { ((get-date)-$_.LastWriteTime).days -lt $MaxAge } `
+  ExcludeDirsRecurseListFilesDirs.ps1 $Path $ExcludeFoders | Where-Object { ((get-date)-$_.LastWriteTime).days -lt $MaxAge } `
   | ForEach-Object {"$($_.LastWriteTime) $($_.FullName)"}
-
 }
