4ff04a1786c1e6b816c92134b613164cfc38ea8b
Ravi S. Iyer <riyer02@gmail.com>
2025-03-11 01:03:53 +0530

    Added README

--- MyPSScripts.ps1
+++ MyPSScripts.ps1
@@ -1,4 +1,4 @@
-Write-Host "These are my active Powershell scripts in cmds folder which is part of PATH env. variable"
+Write-Host "These are my active Powershell scripts in cmds folder which is part of PATH env. variable" `n
 Write-Host "Copy related - using Robocopy"
 Write-Host "============================="
 Write-Host "CopyWoXF: Copy contents of a folder excluding specified/default folders (e.g. node_modules and .next folders)."
@@ -9,10 +9,10 @@ Write-Host "List related"
 Write-Host "============" 
 Write-Host "ListItemWoXF: Output (list) files and folders items (objects) excluding specified/default folders."
 Write-Host "ListWoXF: List files and folders excluding specified/default folders."
-Write-Host "ListAll: List all files and folders. No folders are excluded." `n
+Write-Host "ListAll: List all files and folders. No folders are excluded."
 Write-Host "ListMaxAgeWoXF: List last modified in x days, files and folders OR only unique parent folder names, excluding specified/default folders"
 Write-Host "ListLastModWoXF: List few last modified files and folders excluding specified/default folders."
-Write-Host "FindFldrsWoXF: Find and list folders with names matching passed Find-Folders-List excluding specified/default folders."
+Write-Host "FindFldrsWoXF: Find and list folders with names matching passed Find-Folders-List excluding specified/default folders." `n
 
 Write-Host "Zip and move to MayDeleteLater folder related"
 Write-Host "=============================================" 
---
+++ README.md
@@ -0,0 +1,7 @@
+Powershell scripts mainly for copying and listing software development project source code folders and files
+===========
+
+Run or view MyPSScripts.ps1 in root folder of the repo to get a quick overview of the main scripts.
+
+While the scripts were written primarily to help me do copying and listing of software development project (MERN stack, React Native and Android)
+source code folders and files, it does help for other folders and files too.
