58250311bcc42cd689f8d44a66f16ae8234bcfcf
Ravi S. Iyer <riyer02@gmail.com>
2025-03-13 02:03:30 +0530

    Added MoveToBack

---
+++ ZipMove/MoveToBack.ps1
@@ -0,0 +1,56 @@
+#
+param ($InputFolderOrFile="", $BackupFolder="")
+$BackupFolderDefault = "E:\TempBack"
+
+function Usage {
+  param ($cmdName)
+  Write-Host "Move InputFolderOrFile to BackupFolder"`n
+  Write-Host Usage: $cmdName Input-Folder-Or-File-Name [Backup-Folder]`n
+  Write-Host Backup-Folder is the final copy location. By default it is: $BackupFolderDefault
+  Write-Host /? passed as first parameter shows this help message.`n
+}
+
+if ($InputFolderOrFile -eq "/?") { 
+  Usage $myInvocation.InvocationName
+  exit 0
+}
+
+if ( "" -eq $InputFolderOrFile  ) {
+  write-error "Input folder or file not specified."
+  Usage $myInvocation.InvocationName
+  exit 1
+}
+
+If ( -not (Test-Path -path $InputFolderOrFile)) {
+  Write-Error "Input folder or file parameter specified: '$InputFolderOrFile' does not exist. Aborting!"
+  Usage $myInvocation.InvocationName
+  exit 1
+}
+
+if (( "" -eq $BackupFolder  ) -or ("-" -eq $BackupFolder)) {
+    $BackupFolder = $BackupFolderDefault
+}
+
+$MoveCmd = "Move-Item -Path $InputFolderOrFile -Destination $BackupFolder"
+Write-Host `n"Move command to be executed:"
+Write-Host $MoveCmd
+
+$Choices = [System.Management.Automation.Host.ChoiceDescription[]] @("&yes", "&no")
+$Choice = $host.UI.PromptForChoice("", "Proceed?", $Choices, 1)
+
+if (1 -eq $Choice)
+{
+  Write-Error "Aborted!"
+  exit 1
+} else {
+  try {
+    Invoke-Expression $MoveCmd
+  }
+  catch {
+    Write-Error "Above command threw exception: $($PSItem.ToString())"
+    exit 1
+  }
+}
+
+Write-Host
+exit 0
