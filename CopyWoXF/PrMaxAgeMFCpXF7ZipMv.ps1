907e0ecc36feffd7700b1854918ab1a4aafe21db
Ravi S. Iyer <riyer02@gmail.com>
2025-08-01 20:39:29 +0530

    Added PrMaxAgeMFCpXF7ZipMv

---
+++ CopyWoXF/PrMaxAgeMFCpXF7ZipMv.ps1
@@ -0,0 +1,56 @@
+param (
+    [string]$InputFolder,
+    [hashtable]$ProjectDirsAndTypes
+)
+
+. "$PSScriptRoot\ProjectTypeExcludes.ps1"
+
+function Show-Usage {
+    Write-Host "Usage: `n`n`t$($myInvocation.InvocationName) -InputFolder <path> -ProjectDirsAndTypes <hashtable>" -f Yellow
+    Write-Host "-InputFolder <path>                : The source folder containing the projects. (Mandatory)"
+    Write-Host "-ProjectDirsAndTypes <hashtable>   : A hashtable of directory names and their corresponding project types. (Mandatory)"
+    Write-Host "`nExamples:"
+    Write-Host "`nExample 1: Basic usage with a single project type:"
+    Write-Host "$($myInvocation.InvocationName) -InputFolder 'C:\Users\xyz-user\CurrProj' -ProjectDirsAndTypes @{'ReactNative'='ReactNative'}"
+    Write-Host "`nExample 2: Usage with multiple project types:"
+    Write-Host "$($myInvocation.InvocationName) -InputFolder 'C:\Users\xyz-user\CurrProj' -ProjectDirsAndTypes @{'ReactNative'='ReactNative'; 'DotNet'='DotNet'; 'MERN'='Others'}"
+}
+
+if (-not $InputFolder -or $InputFolder -eq "/?" -or -not $ProjectDirsAndTypes) {
+    Write-Host "Missing one or more mandatory parameters." -f Red
+    Show-Usage
+    exit
+}
+
+Write-Host "In dir: $pwd"
+$MaxAge = Read-Host -Prompt "Please specify MaxAge [1]"
+if ([string]::IsNullOrWhiteSpace($MaxAge)) {
+	$MaxAge = 1
+}
+
+# Loop through each project directory and type from the provided hashtable
+foreach ($ProjDir in $ProjectDirsAndTypes.GetEnumerator()) {
+    $DirName = $ProjDir.Key
+    $ProjectType = $ProjDir.Value
+    $ProjTypeInputFolder = Join-Path -Path $InputFolder -ChildPath $DirName
+
+    # Validate ProjectType
+    if ($ProjectType -notin $validTypes) {
+        Write-Host "Invalid ProjectType specified: '$ProjectType' for Directory '$DirName'"
+        Write-Host "Skipping this directory."
+        continue
+    }
+    $ExcludeFolders = $ProjectTypeExcludes[$ProjectType]
+
+    If (Test-Path -Path $ProjTypeInputFolder -PathType Container) {
+        Write-Host "CpXFZipMv '$ProjTypeInputFolder' $MaxAge Y Y '$ExcludeFolders'"
+        $Choices = [System.Management.Automation.Host.ChoiceDescription[]] @("&yes", "&no")
+        $Choice = $host.UI.PromptForChoice("", "Proceed?", $Choices, 1)
+        if (1 -eq $Choice)
+        {
+            Write-Host "Skipping this directory."
+            continue
+        }
+        CpXFZipMv $ProjTypeInputFolder $MaxAge Y Y "$ExcludeFolders"
+    }
+}
